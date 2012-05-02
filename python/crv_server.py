276d9f9d033b72f10ae2d71690f8b15176447c03
Luigi Calori <l.calori@cineca.it>
2012-05-02 14:56:29 +0000

    module to load are now in profile/advanced

--- python/crv_server.py
+++ python/crv_server.py
@@ -53,7 +53,7 @@ class crv_server:
 
 . /cineca/prod/environment/module/3.1.6/none/init/bash
 module purge
-module use /plx/userprod/pro3dwe1/BA/modulefiles/profiles && module load luigi/advanced && module load autoload VirtualGL && module load TurboVNC
+module load profile/advanced && module load autoload VirtualGL TurboVNC
 vncserver -fg > $CRV_JOBLOG.vnc 2>&1
 """
     self.executable=sys.argv[0]
