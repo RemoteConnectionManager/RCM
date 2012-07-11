a69299f9acc6ef238419f5a20eed11d1ad5d9e9f
Luigi Calori <l.calori@cineca.it>
2012-07-11 12:58:20 +0000

    fixing alias

--- python/Desktop_setup/paraview.sh
+++ python/Desktop_setup/paraview.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+shopt -s expand_aliases
 module load profile/advanced 
 module load autoload VirtualGL
 module load autoload ParaView
 
-vglrun -d :0.$((RANDOM%2+1)) paraview
+vglrun  paraview
