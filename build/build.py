356662e6475f3b873e0fe7a5bad9c1108f79fcf4
Roberto Mucci <muccix@gmail.com>
2012-07-12 13:26:02 +0000

    python script to build application

---
+++ build/build.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+import os
+import tempfile
+import shutil
+
+
+
+build = os.path.dirname(os.path.abspath(__file__))
+root = os.path.dirname(build)
+
+print "Build dir: ", build
+print "Root dir: ", root
+
+#create tmp dir
+tmpDir = tempfile.gettempdir()
+tmpFile = os.path.join(tmpDir,'crv_client_tk.spec')
+print "Tmp dir: ",tmpDir
+
+shutil.copyfile(os.path.join(root,'spec_files','crv_client_tk.spec'), tmpFile)
+
+os.chdir(tmpDir)
+os.system('python '+ os.path.join(root,'PyInstaller','pyinstaller-1.5.1','Build.py') + ' ' + tmpFile)
