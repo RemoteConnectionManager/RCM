f78c5bd23ab488ea248abab525f038be20d624c2
Luigi Calori <l.calori@cineca.it>
2012-07-13 17:15:03 +0000

    some hints to pack and  build dmg under osx ... to be integrated into build.py

--- build/build.py
+++ build/build.py
@@ -20,4 +20,5 @@ print "Tmp dir: ",tmpDir
 shutil.copyfile(os.path.join(root,'spec_files','crv_client_tk.spec'), tmpFile)
 
 os.chdir(tmpDir)
+os.environ['VERSIONER_PYTHON_PREFER_32_BIT']='1'
 os.system('python '+ os.path.join(root,'PyInstaller','pyinstaller-1.5.1','Build.py') + ' ' + tmpFile)
