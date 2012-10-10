187a19b655fad08bb73bccb71b20a068e7b4c7bc
Roberto Mucci <muccix@gmail.com>
2012-10-10 10:34:16 +0000

    fixed md5 calculation and write config file

--- build/release_checksum.py
+++ build/release_checksum.py
@@ -16,7 +16,7 @@ def mychecksum(file):
         data = fh.read(8192)
         if not data:
             break
-    m.update(data)
+        m.update(data)
     return m.hexdigest()
 
 build = os.path.dirname(os.path.abspath(__file__))
@@ -31,13 +31,26 @@ checksum=dict()
 release_dir=os.path.join(build,"dist","Releases")
 for exefile in os.listdir(release_dir):
     print exefile
-    result = re.match(r"^RCM_(.*)$|.exe$", exefile)
+    result = re.match(r"^RCM_(.*)", exefile)
     if(result):
         myplatform = result.group(1)
+        myplatform = myplatform.replace(".exe","")
+        myplatform = myplatform.replace(".dmg","")
         url[myplatform]=baseurl+exefile
         print myplatform+" = "+url[myplatform]
         checksum[myplatform]=mychecksum(os.path.join(release_dir,exefile))
         print myplatform+" = "+checksum[myplatform]
+
+#edit configuration file
+configurationFile = os.path.join(root,"python", "versionRCM.cfg")
+f = open(configurationFile, 'w')
+f.write("[checksum]" + '\n')
+for checksumKey in checksum.keys():
+    f.write(checksumKey+" = "+ checksum[checksumKey] + '\n')
+f.write("[url]" + '\n')
+for urlKey in url.keys():
+    f.write(urlKey+" = "+url[urlKey] + '\n')
+f.close()
 
         
 #create tmp dir
