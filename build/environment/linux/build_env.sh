c3e0af88c0a4d4c2fcde42fbdd8d18b97a536c11
Luigi Calori <l.calori@cineca.it>
2014-05-30 13:21:22 +0000

    first try build python

--- build/environment/linux/build_env.sh
+++ build/environment/linux/build_env.sh
@@ -8,6 +8,9 @@ export INSTALL_DIR=${BASE_DIR}/install
 mkdir -p $SRC_DIR
 mkdir -p $INSTALL_DIR
 
+export PATH=${INSTALL_DIR}/bin:${PATH}
+export LD_LIBRARY_PATH=${INSTALL_DIR}/lib:${LD_LIBRARY_PATH}
+
 cd $SRC_DIR
 echo "Source dir ---> $SRC_DIR"
 echo "Base dir ----> $BASE_DIR"
@@ -41,12 +44,17 @@ fi
 if [ -d ${SRC_DIR}/Python-2.7.6 ]; then
 	echo "Skip ${SRC_DIR}/Python-2.7.6"
 else
-	#download python
-	wget --no-check-certificate http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
+        if [ -s ${SRC_DIR}/Python-2.7.6.tgz ]; then
+	     echo "Skip download of ${SRC_DIR}/Python-2.7.6.tgz"
+        else
+	     #download python
+	     wget --no-check-certificate http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
+        fi
 	tar -xzf Python-2.7.6.tgz
 	cd ${SRC_DIR}/Python-2.7.6
-        ./configure --prefix=$INSTALL_DIR --exec-prefix=$INSTALL_DIR --with-tcl=${SRC_DIR}/tcl8.5.15/unix
+        ./configure  --with-tcltk-includes=${INSTALL_DIR}/include --with-tcltk-libs=${INSTALL_DIR}/lib
 	make 
 	make install
 fi
+python -c "import Tkinter; a=Tkinter.Tk(); print 'fontsystem-->',a.call('::tk::pkgconfig'.'get','fontsystem'); import tkFont; printtkFont.families(); Tkinter._test()"
 
