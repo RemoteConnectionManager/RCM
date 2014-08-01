59231d5f77a67d61e4a54533a0854689186c2e8a
Luigi Calori <l.calori@cineca.it>
2014-08-01 10:42:52 +0000

    adding functions to platformconfig

--- server/platformconfig.py
+++ server/platformconfig.py
@@ -5,7 +5,7 @@ class platformconfig:
     def __init__(self):
         self.confdict=dict()
         self.confdict[('platform','nodepostfix')]=''
-	self.sessions=dict()
+	self.sections=dict()
 	self.options=dict()
 
     def parse(self):
@@ -20,31 +20,62 @@ class platformconfig:
         for s in config.sections():
             for o in config.options(s):
                 self.confdict[(s,o)]=config.get(s, o)
-		self.sessions[s]=self.sessions.get(s,[])+[o]
+		self.sections[s]=self.sections.get(s,[])+[o]
 		self.options[o]=self.options.get(o,[])+[s]
         
     def scheduler(self):
         hostname = socket.gethostname()
-        print "hostname-->"+hostname+"<--"
+        #print "hostname-->"+hostname+"<--"
         scheduler=self.confdict.get(('platform',hostname),'ssh')
         return scheduler
     
     def get_queues(self):
-	return self.sessions['jobscript']
+	return self.sections['jobscript']
+    
+    def get_testjobs(self):
+	tetst_job_scripts=dict()
+	for q in self.get_queues():
+	  script=self.confdict.get(('testjobscript',q),None)
+	  if(script): tetst_job_scripts[q]=script
+	return tetst_job_scripts
 	
     def get_queue(self,queue):
 	q=dict()
 	for tag in self.options.get(queue,[]):
 	  q[tag]=self.confdict.get((tag,queue),None)
 	return q
+	
+    def get_import_scheduler(self):
+	def_sched="ssh"
+	hostname = socket.gethostname()
+	scheduler = self.confdict.get(('platform',hostname),def_sched)
+	if(def_sched == scheduler):
+		session_tag = hostname
+	else:
+		session_tag = scheduler
+	exec("import rcm_server_"+scheduler+" as rcm_scheduler")
+	return (rcm_scheduler,session_tag)
+	
+
 
 
 if __name__ == '__main__':
     p=platformconfig()
     p.parse()
     #print p.confdict
-    print p.sessions
+    print p.sections
     print p.options
     print p.scheduler()
     print p.get_queues()
     print p.get_queue('visual')
+    print p.get_testjobs()
+    subnet='10.139.7'
+    login='rvn03.plx.cineca.it'
+    print "hack login nameson subnet: ",subnet,login,"-->", p.confdict.get((subnet,login),login)
+    subnet='130.186.1'
+    print "hack login nameson subnet: ",subnet,login,"-->", p.confdict.get((subnet,login),login)
+    (sched,s_tag)=p.get_import_scheduler()
+    print "session_tag-->",s_tag
+    print "instance a server"
+    s=sched.rcm_server()
+    print "available queues-->"+str(s.get_queue(p.get_testjobs()))
