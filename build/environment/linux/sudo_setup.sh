07aed3df2ffd119c07d59c228309a2ebd4bb2776
Luigi Calori <l.calori@cineca.it>
2014-09-12 12:42:47 +0000

    adding subversion to required packages

--- build/environment/linux/sudo_setup.sh
+++ build/environment/linux/sudo_setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get install build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline5-dev libssl-dev libdb-dev
 sudo apt-get install libx11-dev
 sudo apt-get install libxft-dev
+sudo apt-get install subversion
 
