071b0ef6ea7d26371a8d8cbbde01c0e82fc41596
Luigi Calori <l.calori@cineca.it>
2014-12-23 16:33:54 +0000

    fix error of cpy linux 32 bit to linux 64 bit

--- build/environment/linux/get_turbovnc_32.sh
+++ build/environment/linux/get_turbovnc_32.sh
@@ -3,4 +3,4 @@ wget http://sourceforge.net/projects/turbovnc/files/1.2.2/turbovnc_1.2.2_i386.de
 mkdir turbovnc_1.2.2_i386
 dpkg -x turbovnc_1.2.2_i386.deb turbovnc_1.2.2_i386
 cd turbovnc_1.2.2_i386
-cp opt/TurboVNC/bin/vncviewer /home/vagrant/devel/RCM/multivnc/client/external/linux2/64bit/bin/vncviewer
+cp opt/TurboVNC/bin/vncviewer /home/vagrant/devel/RCM/multivnc/client/external/linux2/32bit/bin/vncviewer
