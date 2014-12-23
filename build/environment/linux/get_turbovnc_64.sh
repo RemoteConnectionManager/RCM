a1f99aa0bbc48e0cbf651d43b2387b6c7b8d5c85
Luigi Calori <l.calori@cineca.it>
2014-12-23 16:46:18 +0000

    fix swap of 32 and 64 turbovnc clients

--- build/environment/linux/get_turbovnc_64.sh
+++ build/environment/linux/get_turbovnc_64.sh
@@ -1,4 +1,4 @@
-cd build_env
+cd /home/vagrant/devel/RCM/build_env
 wget http://sourceforge.net/projects/turbovnc/files/1.2.2/turbovnc_1.2.2_amd64.deb
 mkdir turbovnc_1.2.2_amd64
 dpkg -x turbovnc_1.2.2_amd64.deb turbovnc_1.2.2_amd64
