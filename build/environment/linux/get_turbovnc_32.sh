8c07b92d75dc7a11707a397386f82b165db32afe
Luigi Calori <l.calori@cineca.it>
2014-12-23 16:05:15 +0000

    added script for download turbovnc client for 32 bit linux

---
+++ build/environment/linux/get_turbovnc_32.sh
@@ -0,0 +1,6 @@
+cd /home/vagrant/devel/RCM/build_env
+wget http://sourceforge.net/projects/turbovnc/files/1.2.2/turbovnc_1.2.2_i386.deb
+mkdir turbovnc_1.2.2_i386
+dpkg -x turbovnc_1.2.2_i386.deb turbovnc_1.2.2_i386
+cd turbovnc_1.2.2_i386
+cp opt/TurboVNC/bin/vncviewer /home/vagrant/devel/RCM/multivnc/client/external/linux2/64bit/bin/vncviewer
