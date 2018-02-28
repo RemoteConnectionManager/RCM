3ec8f4e1ee3394b9b3fe9886e0a520e0e114ef14
AlePini <alepini@email.it>
2018-02-28 16:52:04 +0100

    Shell Java SSH, edited helloworld (was empity), correct pushTask1.sh

--- qtclient/pushTask1.sh
+++ qtclient/pushTask1.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-cd $HOME/Cineca/Task1/javaHelloWorld
-find . -maxdepth 1 ! -name 'src' -type d -exec rm -rf {} +
-cd $HOME/Cineca/Task1/pythonHelloWorld
-find . -maxdepth 1 ! -name "helloworld.py" -delete
-echo -e "\033[0;31mRemoving unecessary direcories\033[0m"
+cd $HOME/Cineca/Task1
 
-cd $HOME/Cineca
-
-git add -A -v Task1
+git add $(git ls-files -m)
 
 echo "Inserire il commmento del commit"
 read COM
