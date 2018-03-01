b7e4f002f1ecb780cfc42abd13b1f374b0c197b7
Luigi Calori <luigi.calori@gmail.com>
2018-03-01 15:37:36 +0100

    create .rcm folder if not exist, same code used in clean_files and get_rcmdirs

--- server/rcm_new_server.py
+++ server/rcm_new_server.py
@@ -23,7 +23,13 @@ if __name__ == '__main__':
     logger = logging.getLogger(logger_name)
     logger.setLevel( "DEBUG"  )
     ch = logging.StreamHandler(sys.stdout)
-    fh = logging.FileHandler(".rcm/logfile", mode='a')
+    username=pwd.getpwuid(os.geteuid())[0]
+    rcmdir=os.path.expanduser("~%s/.rcm" % username)
+    if ( not os.path.isdir(rcmdir) ):
+        os.mkdir(rcmdir)
+        os.chmod(rcmdir,0755)
+    rcmlog=os.path.join(rcmdir,'logfile')
+    fh = logging.FileHandler(rcmlog, mode='a')
     formatter = logging.Formatter('[%(levelname)s] %(asctime)s (%(module)s %(funcName)s) : %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
