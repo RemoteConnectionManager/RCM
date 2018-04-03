76c0817226476bf907b5bff7d3043e8f01f7eb23
Simone Bna <s.bn@cineca.it>
2018-04-03 17:23:09 +0200

    ENH: moved widgets construction in the init_ui function

--- qtclient/rcm_client/gui/display_dialog.py
+++ qtclient/rcm_client/gui/display_dialog.py
@@ -17,11 +17,6 @@ class QDisplayDialog(QDialog):
         self.display_name = ""
         self.display_names = display_names
 
-        self.session_line = QLineEdit(self)
-        self.session_queue_combo = QComboBox(self)
-        self.session_vnc_combo = QComboBox(self)
-        self.display_combo = QComboBox(self)
-
         self.setWindowTitle("New display")
         self.init_ui()
 
@@ -39,30 +34,39 @@ class QDisplayDialog(QDialog):
         session_name.setText('session name:')
 
         grid_layout.addWidget(session_name, 1, 0)
-        grid_layout.addWidget(self.session_line, 1, 1)
+
+        session_line = QLineEdit(self)
+        session_line.setObjectName('session_line')
+        grid_layout.addWidget(session_line, 1, 1)
 
         session_queue = QLabel(self)
         session_queue.setText('Select queue:')
-        self.session_queue_combo.addItems(["12core_40gb_3h_slurm",
+
+        session_queue_combo = QComboBox(self)
+        session_queue_combo.addItems(["12core_40gb_3h_slurm",
                                            "4core_18gb_1h_slurm",
                                            "4core_18gb_3h_slurm"])
         grid_layout.addWidget(session_queue, 2, 0)
-        grid_layout.addWidget(self.session_queue_combo, 2, 1)
+        grid_layout.addWidget(session_queue_combo, 2, 1)
 
         session_vnc = QLabel(self)
         session_vnc.setText('Select wm+vnc:')
-        self.session_vnc_combo.addItems(["fluxbox_turbovnc",
-                                         "xfce_singularity_turbovnc"])
+
+        session_vnc_combo = QComboBox(self)
+        session_vnc_combo.addItems(["fluxbox_turbovnc",
+                                    "xfce_singularity_turbovnc"])
         grid_layout.addWidget(session_vnc, 3, 0)
-        grid_layout.addWidget(self.session_vnc_combo, 3, 1)
+        grid_layout.addWidget(session_vnc_combo, 3, 1)
 
         display_label = QLabel(self)
         display_label.setText('Display size:')
-        self.display_combo.addItems(["1920x1080",
-                                     "full_screen"])
+
+        display_combo = QComboBox(self)
+        display_combo.addItems(["1920x1080",
+                                "full_screen"])
 
         grid_layout.addWidget(display_label, 4, 0)
-        grid_layout.addWidget(self.display_combo, 4, 1)
+        grid_layout.addWidget(display_combo, 4, 1)
 
         # Ok button
         hor_layout = QHBoxLayout()
@@ -96,7 +100,8 @@ class QDisplayDialog(QDialog):
         :return: Return accept signal if the display name is unique
         """
 
-        display_name = str(self.session_line.text())
+        session_line = self.findChild(QLineEdit, 'session_line')
+        display_name = str(session_line.text())
 
         # if the display_name is empty, it is set to a random uuid
         if display_name == "":
