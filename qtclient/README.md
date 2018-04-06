a45b2a6cd5db5acc336d280a5ce990d056c5379d
Eric Pascolo <eric.pascolo@cineca.it>
2018-04-06 12:39:12 +0200

    in rcm add pythonpath set at runtime

--- qtclient/README.md
+++ qtclient/README.md
@@ -5,6 +5,6 @@ RCM is a VNC client for the remote visualization on the CINECA clusters.
 ```sh
 virtualenv -p python3 py3env
 source py3env/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ```
 
--- qtclient/rcm_client/rcm.py
+++ qtclient/rcm_client/rcm.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
 # std lib
 import sys
+import os
 
 # pyqt5
 from PyQt5.QtWidgets import QApplication
+
+# add python path
+pypath,libpath = os.path.split(os.path.split(os.path.realpath(sys.argv[0]))[0])
+sys.path.append(pypath)
 
 # local includes
 from rcm_client.gui.rcm_main_window import RCMMainWindow
