fee8e12526fa710a96f946dd89fa5a8f0339bd97
Luigi Calori <luigi.calori@gmail.com>
2018-11-05 18:28:46 +0100

    port to python3 and pep8 edits

--- rcm/server/baseconfig.py
+++ rcm/server/baseconfig.py
@@ -1,50 +1,50 @@
 import os
 import sys
-import socket
-import ConfigParser
-import enumerate_interfaces
-import string
-import inspect
-import json
+
+if sys.version_info.major == 3:
+    import configparser as ConfigParser
+else:
+    import ConfigParser
+
 from  logger_server import logger
+
 
 class baseconfig:
     def __init__(self): 
 
-        self.confdict=dict()
-        self.sections=dict()
-        self.options=dict()
-        
+        self.confdict = dict()
+        self.sections = dict()
+        self.options = dict()
 
-    def parse(self,configfile=''):
+    def parse(self, configfile=''):
         logger.debug("parse")
         config = ConfigParser.RawConfigParser()
-        if(not configfile):
-            myPath =   os.path.join(
+        if not configfile:
+            myPath = os.path.join(
                         os.path.dirname(
                          os.path.dirname(
                           os.path.abspath(__file__)
                          )
-                        ),'config')
-            myPathOther =   os.path.join(
+                        ), 'config')
+            myPathOther = os.path.join(
                         os.path.dirname(
                          os.path.dirname(sys.argv[0])
-                        ),'config')
-            configfile=os.path.join(myPath, self.filename)
-        logger.debug("config file : "+configfile)
-        if(not os.path.exists(configfile)):
-            print "WARNING FIRST TRY missing platform file -->"+configfile
-            configfile=os.path.join(myPathOther, self.filename)
-            if(not os.path.exists(configfile)):
-                print "WARNING NO WAY missing platform file -->"+configfile
-                return(False)
+                        ), 'config')
+            configfile = os.path.join(myPath, self.filename)
+        logger.debug("config file : " + configfile)
+        if not os.path.exists(configfile):
+            print("WARNING FIRST TRY missing platform file -->" + configfile)
+            configfile = os.path.join(myPathOther, self.filename)
+            if not os.path.exists(configfile):
+                print("WARNING NO WAY missing platform file -->" + configfile)
+                return False
 #    print "parsing configfile-->",configfile
         config.read(configfile)
         for s in config.sections():
             for o in config.options(s):
-                self.confdict[(s,o)]=config.get(s, o)
-                self.sections[s]=self.sections.get(s,[])+[o]
-                self.options[o]=self.options.get(o,[])+[s]
+                self.confdict[(s, o)] = config.get(s, o)
+                self.sections[s] = self.sections.get(s, []) + [o]
+                self.options[o] = self.options.get(o, []) + [s]
 
         logger.debug(self.sections)
 
--- rcm/server/versionconfig.py
+++ rcm/server/versionconfig.py
@@ -1,24 +1,17 @@
 import os
-import sys
-import socket
-import ConfigParser
-import enumerate_interfaces
-import string
-import inspect
-import json 
 from  baseconfig import baseconfig
 from  logger_server import logger
+
 
 class versionconfig(baseconfig):
     def __init__(self): 
         logger.debug("version config init")
         baseconfig.__init__(self)
-        self.filename=os.path.join(os.path.dirname(os.path.abspath(__file__)),'versionRCM.cfg')
+        self.filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'versionRCM.cfg')
         self.parse()
     
-    def get_checksum(self,buildPlatformString=''): 
+    def get_checksum(self, buildPlatformString=''):
         logger.debug("get_checksum")
-        checksum = self.confdict.get(('checksum',buildPlatformString),'')
-        downloadurl = self.confdict.get(('url', buildPlatformString),'')
-        return(checksum,downloadurl)
-
+        checksum = self.confdict.get(('checksum', buildPlatformString), '')
+        downloadurl = self.confdict.get(('url', buildPlatformString), '')
+        return(checksum, downloadurl)
