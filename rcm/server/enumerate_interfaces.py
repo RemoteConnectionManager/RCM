4edf1d2249d6728a44bbb1be6db430e6699c5e37
Luigi Calori <luigi.calori@gmail.com>
2018-11-05 18:07:49 +0100

    upgrade enumerate_interfaces.py to work under python3

--- rcm/server/enumerate_interfaces.py
+++ rcm/server/enumerate_interfaces.py
@@ -1,6 +1,7 @@
 # Use those functions to enumerate all interfaces available on the system using Python.
 # found on <http://code.activestate.com/recipes/439093/#c1>
 
+import sys
 import socket
 import fcntl
 import struct
@@ -8,24 +9,34 @@ import array
 import re
 from  logger_server import logger
 
+
 def all_interfaces():
-    logger.debug("all_interfaces")
-    max_possible = 128  # arbitrary. raise if needed.
-    bytes = max_possible * 32
+    is_64bits = sys.maxsize > 2**32
+    struct_size = 40 if is_64bits else 32
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    names = array.array('B', '\0' * bytes)
-    outbytes = struct.unpack('iL', fcntl.ioctl(
-        s.fileno(),
-        0x8912,  # SIOCGIFCONF
-        struct.pack('iL', bytes, names.buffer_info()[0])
-    ))[0]
+    max_possible = 8 # initial value
+    while True:
+        _bytes = max_possible * struct_size
+        names = array.array('B')
+        for i in range(0, _bytes):
+            names.append(0)
+        outbytes = struct.unpack('iL', fcntl.ioctl(
+            s.fileno(),
+            0x8912,  # SIOCGIFCONF
+            struct.pack('iL', _bytes, names.buffer_info()[0])
+        ))[0]
+        if outbytes == _bytes:
+            max_possible *= 2
+        else:
+            break
     namestr = names.tostring()
-    lst = []
-    for i in range(0, outbytes, 40):
-        name = namestr[i:i+16].split('\0', 1)[0]
-        ip   = namestr[i+20:i+24]
-        lst.append((name, ip))
-    return lst
+    ifaces = []
+    for i in range(0, outbytes, struct_size):
+        iface_name = bytes.decode(namestr[i:i+16]).split('\0', 1)[0]
+        iface_addr = socket.inet_ntoa(namestr[i+20:i+24])
+        ifaces.append((iface_name, iface_addr))
+
+    return ifaces
 
 def format_ip(addr):
     logger.debug("format_ip")
@@ -40,9 +51,13 @@ def external_name(subnet):
   for i in ifs:
     ip=format_ip(i[1])
     if(re.match('^'+subnet,ip)):
-      name=socket.gethostbyaddr(ip)[0]
-      if(name): return name
-  return None    
+        try:
+            name=socket.gethostbyaddr(ip)[0]
+            if(name): return name
+        except Exception as e:
+            logger.warning("ERROR {0}: ".format(e) + "in external_name subnet->" +
+                                  subnet + "< ip->" + ip + "<")
+  return None
 
 
 if __name__ == "__main__":
@@ -53,9 +68,9 @@ if __name__ == "__main__":
   else:
     subnet="127.0.0"
   name=external_name(subnet)
-  if(name): print "name-->"+name+"<--"
+  if(name): print("name-->"+name+"<--")
   ifs = all_interfaces()
   for i in ifs:
-    print "%12s   %s" % (i[0], format_ip(i[1]))
+    print("%12s   %s" % (i[0], format_ip(i[1])))
   for i in ifs:
-    print "%12s   %s" % (i[0], socket.gethostbyaddr(format_ip(i[1]))[0])
+    print("%12s   %s" % (i[0], external_name(format_ip(i[1]))))
