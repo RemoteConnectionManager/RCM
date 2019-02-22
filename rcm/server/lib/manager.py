e111de901f47115ec36e8290071c9e6d9b67d157
Luigi Calori <luigi.calori@gmail.com>
2019-02-22 15:23:41 +0100

    moved widget tree instantiationwithin manager.init

--- rcm/server/lib/manager.py
+++ rcm/server/lib/manager.py
@@ -1,4 +1,5 @@
 import logging
+import logging.config
 import importlib
 import os
 import json
@@ -8,6 +9,7 @@ from collections import OrderedDict
 # set prefix.
 current_file = os.path.realpath(os.path.expanduser(__file__))
 current_prefix = os.path.dirname(os.path.dirname(current_file))
+print("current_prefix", current_prefix)
 current_etc_path = os.path.join(current_prefix, "etc")
 
 import sys
@@ -39,50 +41,59 @@ class ServerManager:
         self.services = dict()
         self.downloads = dict()
 
-    def init(self):
-        # load plugins from a setting file
-        logger.debug('loading plugins in ' + os.path.join(current_etc_path, 'plugins.yaml'))
+    def init(self, config_paths=[]):
 
-        output = hiyapyco.load(
-            os.path.join(current_etc_path, 'plugins.yaml')
-        )
+        # load and merge yaml config from config_paths by loading logging
+        # being a singleton , this first call define  the yaml files that are loaded
+        # subsequent calls, reuse the same info, even if change the list_paths
+        logging.config.dictConfig(config.CascadeYamlConfig(list_paths = config_paths)[['logging_configs']])
 
-        logger.debug('loading download url and checksum in ' + os.path.join(current_etc_path, 'download.yaml'))
-
-        self.downloads = hiyapyco.load(
-            os.path.join(current_etc_path, 'download.yaml')
-        )
+        # load client download info
+        self.downloads = config.CascadeYamlConfig()[['plugins', 'schedulers']]
 
         # load plugins
-        if 'schedulers' in output and output['schedulers']:
-            for scheduler_str in output['schedulers']:
-                try:
-                    module_name, class_name = scheduler_str.rsplit(".", 1)
-                    print("try allocating: ",module_name, class_name)
 
-                    scheduler_class = getattr(importlib.import_module(module_name), class_name)
-                    scheduler_obj = scheduler_class()
-                    print("ALLOCATING: ",class_name)
-                    self.schedulers[class_name] = scheduler_obj
-                    logger.debug('loaded scheduler plugin ' +
-                                 scheduler_obj.__class__.__name__ +
-                                 " - " + scheduler_obj.NAME)
-                except Exception as e:
-                    logger.error("plugin " + scheduler_str + " loading failed")
-                    logger.error(e)
+        for scheduler_str in config.CascadeYamlConfig()[['plugins', 'schedulers']]:
+            try:
+                module_name, class_name = scheduler_str.rsplit(".", 1)
+                print("try allocating: ",module_name, class_name)
+
+                scheduler_class = getattr(importlib.import_module(module_name), class_name)
+                scheduler_obj = scheduler_class()
+                print("ALLOCATING: ",class_name)
+                self.schedulers[class_name] = scheduler_obj
+                logger.debug('loaded scheduler plugin ' +
+                             scheduler_obj.__class__.__name__ +
+                             " - " + scheduler_obj.NAME)
+            except Exception as e:
+                logger.error("plugin " + scheduler_str + " loading failed")
+                logger.error(e)
 
         # load services
-        if 'services' in output and output['services']:
-            for service_str in output['services']:
-                try:
-                    module_name, class_name = service_str.rsplit(".", 1)
-                    service_class = getattr(importlib.import_module(module_name), class_name)
-                    service_obj = service_class()
-                    self.services[class_name] = service_obj
-                    logger.debug('loaded service plugin ' + service_obj.__class__.__name__ + " - " + service_obj.name)
-                except Exception as e:
-                    logger.error("plugin loading failed")
-                    logger.error(e)
+
+        for service_str in config.CascadeYamlConfig()[['plugins', 'services']]:
+            try:
+                module_name, class_name = service_str.rsplit(".", 1)
+                service_class = getattr(importlib.import_module(module_name), class_name)
+                service_obj = service_class()
+                self.services[class_name] = service_obj
+                logger.debug('loaded service plugin ' + service_obj.__class__.__name__ + " - " + service_obj.name)
+            except Exception as e:
+                logger.error("plugin loading failed")
+                logger.error(e)
+
+        # instantiate widget tree
+        class_table = dict()
+        plug_instances = dict()
+        for scheduler in self.schedulers:
+            plug_instances[self.schedulers[scheduler].NAME] = self.schedulers[scheduler]
+        class_table['SCHEDULER'] = (jobscript_builder.ConnectedManager, plug_instances)
+
+        # SchedulerManager.register_scheduler([SlurmScheduler, PBSScheduler, LocalScheduler])
+        self.widget_root = jobscript_builder.AutoChoiceNode(name='TOP',
+                              # schema=config.conf['schema']['TOP'],
+                              # defaults=config.conf.get('defaults', OrderedDict()).get('TOP',None),
+                              class_table=class_table)
 
     def get_checksum_and_url(self, build_platform):
         checksum = ""
@@ -99,30 +110,13 @@ class ServerManager:
 
         return checksum, downloadurl
 
+    def get_gui_options(self):
+        return self.widget_root.get_gui_options()
+
     def get_jobscript_json_menu(self):
-        SchedulerManager.register_scheduler([
-            scheduler.SlurmScheduler,
-            scheduler.PBSScheduler,
-            scheduler.LocalScheduler])
+        return json.dumps(self.get_gui_options())
 
-        list_paths = []
-        self.config_path = ""
-        import socket
-        nodelogin = socket.gethostname()
-        self.config_path_other = ""
-        home_config_dir = os.path.join(os.environ.get('HOME', ''), '.rcm', 'config')
-        for basepath in [home_config_dir, self.config_path, self.config_path_other]:
-            list_paths.append(os.path.join(basepath, 'config.yaml'))
-            list_paths.append(os.path.join(basepath, nodelogin + '.yaml'))
-
-        self.cascade_config = config.CascadeYamlConfig(list_paths)
-
-        gui_composer = jobscript_builder.AutoChoiceNode(
-            schema=self.cascade_config.conf['schema'],
-            defaults=self.cascade_config.conf.get('defaults', None),
-            class_table={'SCHEDULER': SchedulerManager})
-
-        display_dialog_ui = gui_composer.get_gui_options()
-        return json.dumps(display_dialog_ui)
+    def substitute(self,choices):
+        return self.widget_root.substitute(choices)
 
 
