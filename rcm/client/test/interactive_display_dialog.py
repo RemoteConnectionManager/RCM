d1cf4ffbba353032a123ec810efc893f34ef3e16
Luigi Calori <luigi.calori@gmail.com>
2019-04-17 17:36:24 +0200

    STYLE: clean comments and logs

--- rcm/client/test/interactive_display_dialog.py
+++ rcm/client/test/interactive_display_dialog.py
@@ -1,4 +1,3 @@
-
 # stdlib
 import json
 import sys
@@ -10,11 +9,11 @@ from PyQt5.QtWidgets import QLabel, QLineEdit, QDialog, QComboBox, \
     QHBoxLayout, QVBoxLayout, QPushButton, \
     QApplication, QTabWidget, QWidget, QSlider, QSizePolicy, QFrame
 
-
 app = QApplication(sys.argv)
 
 import os
 import sys
+
 root_rcm_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_rcm_path)
 sys.path.append(os.path.join(root_rcm_path, 'utils'))
@@ -24,8 +23,6 @@ sys.path.append(os.path.join(root_rcm_path, 'client'))
 import logging
 import os
 
-
-#from server.lib.config import *
 from server.lib.jobscript_builder import *
 from server.lib.scheduler import *
 from server.lib.manager import *
@@ -33,52 +30,41 @@ import config
 
 from gui.new_display_dialog import *
 
-#This is needed, otherwise no default logging happen
+# This is needed, otherwise no default logging happen
 logging.debug("Start test app")
 logger = logging.getLogger('RCM.test_gui')
 logger.setLevel(logging.INFO)
 
-print("sys.argv",sys.argv[1:])
+print("sys.argv", sys.argv[1:])
 
-
-# test_base_path = os.path.join(root_rcm_path, 'server', 'test', 'etc')
-# list_paths=[]
-# for path in sys.argv[1:]:
-#     path = os.path.join(test_base_path, path)
-#     if os.path.exists(path):
-#         list_paths.append(path)
-#     else:
-#         print("WARNING path ", path, "not found")
-# #list_paths.append(os.path.join(os.environ.get('HOME',''), '.rcm', 'config', 'config.yaml'))                                CascadeYamlConfig.get()
-
-# set up default test environment if not used
-
-config.getConfig( )
+config.getConfig()
 manager = ServerManager()
 manager.init()
 
 display_dialog_ui = manager.root_node.get_gui_options()
 logger.debug("-----------------------------------")
 logger.debug(json.dumps(display_dialog_ui, indent=4))
-with open(os.path.join(os.environ['HOME'],'.rcm','display_dialog.json'), 'w') as f:
+with open(os.path.join(os.environ['HOME'], '.rcm', 'display_dialog.json'), 'w') as f:
     f.write(json.dumps(display_dialog_ui, indent=4))
 
 logger.debug("-----------------------------------")
+
 
 def print_result(choices):
     choices_string = json.dumps(choices, indent=4)
     logger.debug(choices_string)
     manager.handle_choices(choices_string)
     manager.create_session()
-    # SchedulerManager._allInstances[0].active_scheduler()
+
     for k, v in manager.top_templates.items():
-        logger.debug(k + " :::>\n" + str(v) +"\n<:::::::::::::::::::::")
+        logger.debug(k + " :::>\n" + str(v) + "\n<")
 
     for sched in manager.schedulers:
         for templ in manager.schedulers[sched].templates:
             logger.debug("@@@@scheduler " + str(sched) + str(templ) + ":::>\n" +
                          str(manager.schedulers[sched].templates[templ]) +
-                         "\n<:::::::::::::::::::::")
+                         "\n<")
+
 
 display_dialog = QDisplayDialog(display_dialog_ui, callback=print_result)
 display_dialog.show()
