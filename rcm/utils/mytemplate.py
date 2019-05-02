537570e91309326996953c770cfa0d7a0c520213
Luigi Calori <luigi.calori@gmail.com>
2019-05-02 16:00:47 +0200

    STYLE: pycharm automatic code reformat

--- rcm/utils/mytemplate.py
+++ rcm/utils/mytemplate.py
@@ -3,10 +3,11 @@ import re
 import string
 import os
 
+
 class stringtemplate(string.Template):
-    def __init__(self,s=''):
-        string.Template.__init__(self,s)
-        rr="""
+    def __init__(self, s=''):
+        string.Template.__init__(self, s)
+        rr = """
          \@(?:
           (?P<escaped>\@) |   # Escape sequence of two delimiters
           (?P<named>[_a-z][_a-z0-9]*)      |   # delimiter and a Python identifier
@@ -14,37 +15,36 @@ class stringtemplate(string.Template):
           (?P<invalid>\@)              # Other ill-formed delimiter exprs
          )
         """
-        self.pattern=re.compile(rr, re.VERBOSE | re.IGNORECASE)
-        self.delimiter='@'
+        self.pattern = re.compile(rr, re.VERBOSE | re.IGNORECASE)
+        self.delimiter = '@'
 
-    def templ_match(self,s=''):
-        m=self.pattern.search(s)
+    def templ_match(self, s=''):
+        m = self.pattern.search(s)
         return m
 
+
 class filetemplate(stringtemplate):
-    def __init__(self,file=''):
+    def __init__(self, file=''):
         if os.path.exists(file):
-            s=file.read()
-            stringtemplate.__init__(self,s)
+            s = file.read()
+            stringtemplate.__init__(self, s)
         else:
-            print("file", file,"not found")
+            print("file", file, "not found")
             return None
-
-
 
 
 if __name__ == '__main__':
 
-    d=dict()
-    d["general"]="BAD_SUBSTITUTE"
-    d['PKG_WORK_DIR']="${BA_PKG_WORK_DIR}"
-    d["general.download"]="http://www.my.url/pippo.tar.gz"
-    d["DOWNLOAD_URL"]="$DOWNLOAD_URL"
-    d["PKG_SOURCE_DIR"]="${BA_PKG_SOURCE_DIR}"
-    d["uu.pp"]="BAD_SUBSTITUTE"
-    d["uu"]="OK_SUBSTITUTE"
-    d["dddd"]="OK_SUBSTITUTE"
-    download="""
+    d = dict()
+    d["general"] = "BAD_SUBSTITUTE"
+    d['PKG_WORK_DIR'] = "${BA_PKG_WORK_DIR}"
+    d["general.download"] = "http://www.my.url/pippo.tar.gz"
+    d["DOWNLOAD_URL"] = "$DOWNLOAD_URL"
+    d["PKG_SOURCE_DIR"] = "${BA_PKG_SOURCE_DIR}"
+    d["uu.pp"] = "BAD_SUBSTITUTE"
+    d["uu"] = "OK_SUBSTITUTE"
+    d["dddd"] = "OK_SUBSTITUTE"
+    download = """
      -- @@--{pp.qq} -- aa@uu.pp @dddd@
      cd @{PKG_WORK_DIR}
      file=`python -c "import os; import sys;  print os.path.basename(sys.argv[1])" @DOWNLOAD_URL`
@@ -59,31 +59,31 @@ if __name__ == '__main__':
     print("---original--")
     print(download)
     print("-----------subst-----------")
-    out=stringtemplate(download).safe_substitute(d)
+    out = stringtemplate(download).safe_substitute(d)
     print(out)
-    m=stringtemplate().templ_match(out)
+    m = stringtemplate().templ_match(out)
     if m:
         print("----unmatched----")
         print(m.group())
     else:
         print("----------------------")
 
-    bad=filetemplate("non_existing_file")
-    print("----",bad)
+    bad = filetemplate("non_existing_file")
+    print("----", bad)
 
     exit()
-    r= stringtemplate().pattern
+    r = stringtemplate().pattern
     print(r.pattern)
-    dd=dict()
+    dd = dict()
     for i in d:
-      dd[i]=d[i]
-      mi=r.search(download)  
-      if mi :
-        print("matched input at pos ",mi.start())
-        t=stringtemplate(download)
-        out=t.safe_substitute(dd)
-        print(out)
-        mo=r.search(out)  
-        if mo :
-            print("matched output at pos ",mo.start())
-            print(mo.groupdict())
+        dd[i] = d[i]
+        mi = r.search(download)
+        if mi:
+            print("matched input at pos ", mi.start())
+            t = stringtemplate(download)
+            out = t.safe_substitute(dd)
+            print(out)
+            mo = r.search(out)
+            if mo:
+                print("matched output at pos ", mo.start())
+                print(mo.groupdict())
