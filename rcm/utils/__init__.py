6889c354763f6aec675740d24d7ed25b420d26a4
Luigi Calori <luigi.calori@gmail.com>
2019-05-10 18:12:26 +0200

    FIX TRY remove hopefully unneeded imports

--- rcm/utils/__init__.py
+++ rcm/utils/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 
 from .misc import StringTemplate, notimeleft_string, timeleft_string
 from .external import hiyapyco
-from . import error
-from .executable import which
+#from . import error
+#from .executable import which
