8483740a40bd84b3e5a5544d24e46c63249a81bb
Luigi Calori <l.calori@cineca.it>
2023-01-26 13:20:12 +0100

    fix pyinstaller packaging error due to change api in python from 3.10, see https://stackoverflow.com/questions/69381312/in-vs-code-importerror-cannot-import-name-mapping-from-collections/69727802#69727802

--- rcm/utils/external/jinja2/tests.py
+++ rcm/utils/external/jinja2/tests.py
@@ -9,7 +9,10 @@
     :license: BSD, see LICENSE for more details.
 """
 import re
-from collections import Mapping
+try:
+    from collections.abc import Mapping
+except ImportError:
+    from collections import Mapping
 from jinja2.runtime import Undefined
 from jinja2._compat import text_type, string_types, integer_types
 import decimal
