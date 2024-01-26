b48307d0aac46db66fc1b350f2ceddd6b7869dbb
agiunch1 <a.giunchi@cineca.it>
2024-01-26 09:07:49 +0100

    ci: checkout is required by venv cache name, possibility to pass arguments to install python scripts

--- scripts/ci/install_local_python.ps1
+++ scripts/ci/install_local_python.ps1
@@ -1,13 +1,31 @@
-Set-StrictMode -Version Latest
+param
+(
+    [string]$PYTHON_VERSION = $env:PYTHON_VERSION
+)
+    
+$ErrorActionPreference = "Stop"
 
+if ($PYTHON_VERSION -eq '') {
+    throw "PYTHON_VERSION has not been passed or set in the environment"
+}
+
+
+if (Test-Path -Path .\.pyenv ) {
+    Remove-Item .\.pyenv\ -Recurse -Force
+}
 git clone "https://github.com/pyenv-win/pyenv-win.git" .pyenv
-.\.pyenv\pyenv-win\bin\pyenv.bat install ${env:PYTHON_VERSION}
+
+.\.pyenv\pyenv-win\bin\pyenv.bat install ${PYTHON_VERSION}
 
 #############################################
 # TO BE REMOVED IF ARTIFACT ARE NOT REQUIRED
 #############################################
-mv .pyenv\pyenv-win\versions\${env:PYTHON_VERSION} python
-ls python
+if (Test-Path -Path .\python ) {
+    Remove-Item .\python -Recurse -Force
+}
+
+Move-Item .pyenv\pyenv-win\versions\${PYTHON_VERSION} python
+Get-ChildItem python
 #############################################
 
 python\python.exe --version
--- scripts/ci/install_local_venv.ps1
+++ scripts/ci/install_local_venv.ps1
@@ -1,4 +1,8 @@
-Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+if (Test-Path -Path .\venv ) {
+    Remove-Item .\venv -Recurse -Force
+}
 
 python\python.exe -m venv venv
 venv\Scripts\Activate.ps1
