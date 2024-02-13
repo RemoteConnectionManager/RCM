12946e9f266772091c88cac5414b1cd550709fa7
Giuncan <38205328+Giuncan@users.noreply.github.com>
2024-02-13 15:27:08 +0100

    fix macos and echo windows

--- scripts/ci/extract_turbovnc.ps1
+++ scripts/ci/extract_turbovnc.ps1
@@ -1,6 +1,7 @@
 Set-StrictMode -Version Latest
 
 # Download exe
+Write-Host "Invoke-WebRequest -URI \"${env:TURBOVNC_DOWNLOAD}/${env:TURBOVNC_VERSION}/TurboVNC-${env:TURBOVNC_VERSION}-x64.exe\" -OutFile turbovnc.exe"
 Invoke-WebRequest -URI "${env:TURBOVNC_DOWNLOAD}/${env:TURBOVNC_VERSION}/TurboVNC-${env:TURBOVNC_VERSION}-x64.exe" -OutFile turbovnc.exe 
 
 # Extract file from exe
