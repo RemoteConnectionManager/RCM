8c0b7706edc55d616f04c9d2adc62420b0ba59fe
Giuncan <andrea.giunchi.93@gmail.com>
2024-03-05 15:09:28 +0100

    ci: clean old workflow

--- scripts/ci/install_local_git.ps1
+++
@@ -1,8 +0,0 @@
-Set-StrictMode -Version Latest
-
-# https://www.7-zip.org/a/7zr.exe
-choco install --no-progress 7zip
-$URL = "${env:GIT_DOWNLOAD}/v${env:GIT_VERSION}.windows.1/PortableGit-${env:GIT_VERSION}-64-bit.7z.exe"
-Write-Output $URL
-Invoke-WebRequest -URI $URL -OutFile git.7zip
-7z.exe x -ogit git.7zip
