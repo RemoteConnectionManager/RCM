d7c0991286ae931bf98637d7cb48a978d7e2319e
Giuncan <andrea.giunchi.93@gmail.com>
2024-03-14 16:03:45 +0100

    ci: update setup

--- scripts/ci/setup.ps1
+++ scripts/ci/setup.ps1
@@ -1,4 +1,47 @@
+param(
+    [Parameter(HelpMessage="Clean actual setup without reinstalling it (python, venv, externals) [default: False]")]
+    [switch]$CleanOnly,
+
+    [Parameter(HelpMessage="Restart installation without cleaning up [default: False]")]
+    [switch]$Restart
+)
+
 $ErrorActionPreference = 'Stop'
+
+if ($CleanOnly -and $Restart) {
+    Write-Error "Error: '-CleanOnly' and '-Restart' cannot be use at the same time."
+    exit 1
+}
+
+$SEPARATOR = "#"*79
+
+function RunStep {
+
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Step title")]
+        [string]$Title,
+        
+        [Parameter(HelpMessage="Script to run")]
+        [string]$Script,
+
+        [Parameter(HelpMessage="Target dir to eventually clean")]
+        [string]$TargetDir
+    )
+    Write-Host ("{0}`n# {1}`n{0}" -f "${SEPARATOR}", "${Title}" ) -ForegroundColor Blue
+
+    if ($PSBoundParameters.ContainsKey('Script')) {
+        if (-Not ${Restart} -and $PSBoundParameters.ContainsKey('TargetDir') -and (test-path $TargetDir)){
+            Write-Host "[INFO] Removing '${TargetDir}' dir ..." -ForegroundColor DarkGray
+            Remove-Item "${TargetDir}" -Force -Recurse
+        }
+        if ( -Not ${CleanOnly} ) {
+            Write-Host "[INFO] Running '${Script}' ..."  -ForegroundColor DarkGray
+            . "${Script}"
+        }
+        Write-Host "Success" -ForegroundColor Green
+    }
+}
+
 
 $env:TURBOVNC_VERSION = '3.1'
 $env:PYTHON_VERSION = "3.10.11"
@@ -20,52 +63,30 @@ $env:SMALLSTEP_DOWNLOAD = "https://github.com/smallstep/cli/releases/download"
 $env:SMALLSTEP_VERSION = "0.25.2"
 $env:SMALLSTEP_EXTERNAL = "rcm/client/external/step"
 
-
 # CD in RCM directory
 $env:RCM_CHECKOUT = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
 
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 1) SETUP ENVIRONMENT"  -ForegroundColor Blue
-Write-Host "##############################################################################" -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\01-setup-pyenv.ps1"
-Write-Host "Success" -ForegroundColor Green
+RunStep -Title "1) SETUP ENVIRONMENT" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\01-setup-pyenv.ps1" `
+        -TargetDir "${env:RCM_CHECKOUT}\tmp"
 
+RunStep -Title "2) INSTALLING PYENV LOCALLY" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\02-install-python.ps1" `
+        -TargetDir "${env:RCM_CHECKOUT}\.pyenv" 
 
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 2) INSTALLING PYENV LOCALLY" -ForegroundColor Blue
-Write-Host "##############################################################################" -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\02-install-python.ps1"
-Write-Host "Success" -ForegroundColor Green
+RunStep -Title "3) INSTALLING VENV AND PATCH PARAMIKO" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\03-install-venv.ps1" `
+        -TargetDir "${env:RCM_CHECKOUT}\.venv" 
 
+RunStep -Title "4a) DOWNLOAD AND EXTRACT TURBOVNC" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\04a-extract-turbovnc.ps1" `
+        -TargetDir "${env:RCM_CHECKOUT}\rcm\client\external\turbovnc" 
 
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 3) INSTALLING VENV AND PATCH PARAMIKO" -ForegroundColor Blue
-Write-Host "##############################################################################" -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\03-install-venv.ps1"
-Write-Host "Success" -ForegroundColor Green
+RunStep -Title "4b) INSTALLING PYENV LOCALLY" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\04b-patch-turbovnc.ps1"
 
+RunStep -Title "5) DOWNLOAD AND EXTRACT STEP" `
+        -Script "${env:RCM_CHECKOUT}\scripts\ci\05-extract-step.ps1" `
+        -TargetDir "${env:RCM_CHECKOUT}\rcm\client\external\step" 
 
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 4a) DOWNLOAD AND EXTRACT TURBOVNC" -ForegroundColor Blue
-Write-Host "##############################################################################"  -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\04a-extract-turbovnc.ps1"
-Write-Host "Success" -ForegroundColor Green
-
-
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 4b) PATCH TURBOVNC" -ForegroundColor Blue
-Write-Host "##############################################################################" -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\04b-patch-turbovnc.ps1"
-Write-Host "Success" -ForegroundColor Green
-
-Write-Host "##############################################################################" -ForegroundColor Blue
-Write-Host "# 5) DOWNLOAD AND EXTRACT STEP" -ForegroundColor Blue
-Write-Host "##############################################################################" -ForegroundColor Blue
-. "${env:RCM_CHECKOUT}\scripts\ci\05-extract-step.ps1"
-Write-Host "Success" -ForegroundColor Green
-
-Write-Host "##############################################################################" -ForegroundColor Green
-Write-Host "# ENVIRONMENT SETUP COMPLETED" -ForegroundColor Green
-Write-Host "##############################################################################" -ForegroundColor Green
-
-exit
+Write-Host ("{0}`n# {1}`n{0}" -f "${SEPARATOR}", "ENVIRONMENT SETUP COMPLETED" ) -ForegroundColor Green
--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,6 +1,66 @@
 #! /usr/bin/env bash
 set -e
 
+usage() {
+    cat >&1 <<EOF
+
+Usage: $0 [--silent]
+       $0 --restart [--silent]
+       $0 --clean-only
+
+Options:
+  -u | --clean-only   : Clean actual setup without reinstalling it (python, venv, externals) [default: False]
+  -r | --restart      : Restart installation without cleaning up [default: False]
+  -s | --silent       : Suppress command stdout
+  -h | --help         : Print usage message
+
+EOF
+}
+
+
+options=$(getopt -o hscr --long help,silent,clean-only,restart -- "$@")
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -h | --help) usage 0; exit 0;;
+    -s | --silent) SILENT=1 && shift ;;
+    -c | --clean-only) CLEAN_ONLY=1 && shift ;;
+    -r | --restart) RESTART=1 && shift ;;
+    --) shift && break ;;
+    *) usage ;;
+    esac
+done
+
+if [[ "${CLEAN_ONLY}" -eq 1 ]] && [[ "${RESTART}" -eq 1 ]]; then
+    printf "\033[1;31m%s\033[0m\n" "Error: '--clean-only' and '--restart' cannot be use at the same time." 1>&2
+    exit 1
+fi
+
+SEPARATOR="##############################################################################"
+function run_step {
+    TITLE=$1
+    SCRIPT=$2
+    TARGET_DIR=$3
+
+    printf '\033[1;34m%s\n# %s\n%s\033[0m\n' "${SEPARATOR}" "${TITLE}" "${SEPARATOR}"
+
+    if [ -n "${SCRIPT}" ]; then
+        if [[ "${RESTART}" -ne 1 ]] && [[ -d "${TARGET_DIR}" ]]; then
+            printf "\e[90m[INFO] Removing '%s' dir ...\033[0m\n" "${TARGET_DIR}"
+            rm -rf "${TARGET_DIR}"
+        fi
+        if [[ "${CLEAN_ONLY}" -ne 1 ]]; then
+            printf "\e[90m[INFO] Running '%s' ...\033[0m\n" "${SCRIPT}"
+            if [[ "${SILENT}" -eq 1 ]]; then
+                "${SCRIPT}" > /dev/null
+            else
+                "${SCRIPT}"
+            fi
+        fi
+        echo -e "\033[0;32mSuccess\033[0m"
+    fi
+}
+
 export TURBOVNC_VERSION='3.1'
 export PYTHON_VERSION="3.10.11"
 
@@ -24,45 +84,29 @@ export SMALLSTEP_EXTERNAL="rcm/client/external/step"
 
 # CD in RCM directory
 RCM_CHECKOUT="$(dirname "$0")/../../"
+export RCM_CHECKOUT
 
-echo -e "\033[0;34m##############################################################################"
-echo -e "# 1) SETUP PYENV ENVIRONMENT"
-echo -e "##############################################################################\033[0m"
-source "${RCM_CHECKOUT}/scripts/ci/01-setup-pyenv.sh"
-echo -e "\033[0;32mSuccess\033[0m"
+run_step "1) SETUP ENVIRONMENT" \
+         "${RCM_CHECKOUT}/scripts/ci/01-setup-pyenv.sh" \
+         "${RCM_CHECKOUT}/tmp"
 
-echo -e "\033[0;34m##############################################################################"
-echo -e "# 2) INSTALLING PYENV LOCALLY"
-echo -e "##############################################################################\033[0m"
-. "${RCM_CHECKOUT}/scripts/ci/02-install-python.sh"
-echo -e "\033[0;32mSuccess\033[0m"
+run_step "2) INSTALLING PYENV LOCALLY" \
+         "${RCM_CHECKOUT}/scripts/ci/02-install-python.sh" \
+         "${RCM_CHECKOUT}/.pyenv" 
 
-echo -e "\033[0;34m##############################################################################"
-echo -e "# 3) INSTALLING VENV AND PATCH PARAMIKO"
-echo -e "##############################################################################\033[0m"
-. "${RCM_CHECKOUT}/scripts/ci/03-install-venv.sh"
-echo -e "\033[0;32mSuccess\033[0m"
+run_step "3) INSTALLING VENV AND PATCH PARAMIKO" \
+         "${RCM_CHECKOUT}/scripts/ci/03-install-venv.sh" \
+         "${RCM_CHECKOUT}/.venv" 
 
-if uname -a | grep -q "^Linux"; then
-    echo -e "\033[0;34m##############################################################################"
-    echo -e "# 4a) DOWNLOAD AND EXTRACT TURBOVNC (LINUX-ONLY)"
-    echo -e "##############################################################################\033[0m"
-    . "${RCM_CHECKOUT}/scripts/ci/04a-extract-turbovnc-ubuntu.sh"
-    echo -e "\033[0;32mSuccess\033[0m"
+run_step "4a) DOWNLOAD AND EXTRACT TURBOVNC" \
+         "${RCM_CHECKOUT}/scripts/ci/04a-extract-turbovnc-ubuntu.sh" \
+         "${RCM_CHECKOUT}/rcm/client/external/turbovnc" 
 
-    echo -e "\033[0;34m##############################################################################"
-    echo -e "# 4b) PATCH TURBOVNC (LINUX-ONLY)"
-    echo -e "##############################################################################\033[0m"
-    . "${RCM_CHECKOUT}/scripts/ci/04b-patch-turbovnc-linux.sh"
-    echo -e "\033[0;32mSuccess\033[0m"
+run_step "4b) INSTALLING PYENV LOCALLY" \
+         "${RCM_CHECKOUT}/scripts/ci/04b-patch-turbovnc-linux.sh"
 
-    echo -e "\033[0;34m##############################################################################"
-    echo -e "# 5) DOWNLOAD AND EXTRACT STEP (LINUX-ONLY)"
-    echo -e "##############################################################################\033[0m"
-    . "${RCM_CHECKOUT}/scripts/ci/05-extract-step-linux.sh"
-    echo -e "\033[0;32mSuccess\033[0m"
-fi
+run_step "5) DOWNLOAD AND EXTRACT STEP" \
+         "${RCM_CHECKOUT}/scripts/ci/05-extract-step-linux.sh" \
+         "${RCM_CHECKOUT}/rcm/client/external/step" 
 
-echo -e "\033[0;32m##############################################################################"
-echo -e "# ENVIRONMENT SETUP COMPLETED"
-echo -e "##############################################################################\033[0m"
+printf '\033[0;32mm%s\n# %s\n%s\033[0m\n' "${SEPARATOR}" "ENVIRONMENT SETUP COMPLETED" "${SEPARATOR}"
