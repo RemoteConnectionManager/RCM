8e56766404d86820c6ed34676c6d67d00b944ef3
giuncan <andrea.giunchi.93@gmail.com>
2024-09-18 14:54:45 +0200

    ci: replace dpkg-deb with ar and tar and ignore .pyenv

--- scripts/ci/04a-extract-turbovnc-ubuntu.sh
+++ scripts/ci/04a-extract-turbovnc-ubuntu.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 
-mkdir -p "${RCM_CHECKOUT:?}/tmp"
+mkdir -p "${RCM_CHECKOUT:?}/tmp/turbovnc"
 
 # Download
 wget "${TURBOVNC_DOWNLOAD:?}/${TURBOVNC_VERSION:?}/turbovnc_${TURBOVNC_VERSION:?}_amd64.deb" -O "${RCM_CHECKOUT:?}/tmp/turbovnc.deb"
 
-# Extract
-dpkg-deb -x "${RCM_CHECKOUT:?}/tmp/turbovnc.deb" "${RCM_CHECKOUT:?}/tmp/turbovnc"
+# Extract without using dpkg-deb that is distributed only in debian-based distro
+# dpkg-deb -x "${RCM_CHECKOUT:?}/tmp/turbovnc.deb" "${RCM_CHECKOUT:?}/tmp/turbovnc"
+ar -x "${RCM_CHECKOUT:?}/tmp/turbovnc.deb" --output "${RCM_CHECKOUT:?}/tmp"
+tar -xvf "${RCM_CHECKOUT:?}/tmp/data.tar.xz" -C "${RCM_CHECKOUT:?}/tmp/turbovnc"
 
 # Copy extracted file to `"${TURBOVNC_EXTERNAL}"`
 rm -rf "${RCM_CHECKOUT:?}/${TURBOVNC_EXTERNAL:?}"
