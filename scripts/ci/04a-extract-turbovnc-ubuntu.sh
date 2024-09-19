9b039732a7ec1ad01fb90c2462caebca6a4fbaf9
giuncan <andrea.giunchi.93@gmail.com>
2024-09-19 15:48:45 +0200

    ci: fix data.tar that could be xz or gz

--- scripts/ci/04a-extract-turbovnc-ubuntu.sh
+++ scripts/ci/04a-extract-turbovnc-ubuntu.sh
@@ -8,8 +8,8 @@ wget "${TURBOVNC_DOWNLOAD:?}/${TURBOVNC_VERSION:?}/turbovnc_${TURBOVNC_VERSION:?
 # Extract without using dpkg-deb that is distributed only in debian-based distro
 # dpkg-deb -x "${RCM_CHECKOUT:?}/tmp/turbovnc.deb" "${RCM_CHECKOUT:?}/tmp/turbovnc"
 ar -x "${RCM_CHECKOUT:?}/tmp/turbovnc.deb" --output "${RCM_CHECKOUT:?}/tmp"
-ls "${RCM_CHECKOUT:?}/tmp"
-tar -xvf "${RCM_CHECKOUT:?}/tmp/data.tar.xz" -C "${RCM_CHECKOUT:?}/tmp/turbovnc"
+data_tar=$(ls "${RCM_CHECKOUT:?}/tmp/data.tar"*)
+tar $(file "$data_tar" | grep -q "gzip" && echo "-xzvf" || echo "-xvf" ) "$data_tar" -C "${RCM_CHECKOUT:?}/tmp/turbovnc"
 
 # Copy extracted file to `"${TURBOVNC_EXTERNAL}"`
 rm -rf "${RCM_CHECKOUT:?}/${TURBOVNC_EXTERNAL:?}"
