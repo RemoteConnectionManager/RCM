b7c84ce6b92ad41f7e104327948defe89a8265ec
giuncan <andrea.giunchi.93@gmail.com>
2024-09-19 12:15:03 +0200

    ci: remove innoextract step and run turbovnc exe silently

--- scripts/ci/04a-extract-turbovnc.ps1
+++ scripts/ci/04a-extract-turbovnc.ps1
@@ -2,17 +2,18 @@ $ErrorActionPreference = 'Stop'
 
 New-Item -ItemType Directory -Path "${env:RCM_CHECKOUT}\tmp" -Force
 
-# Install innoextract locally
-Invoke-WebRequest -URI "https://github.com/dscharrer/innoextract/releases/download/1.9/innoextract-1.9-windows.zip" -OutFile "${env:RCM_CHECKOUT}\tmp\innoextract.zip"
-Expand-Archive -LiteralPath "${env:RCM_CHECKOUT}\tmp\innoextract.zip" -DestinationPath "${env:RCM_CHECKOUT}\tmp\innoextract" -Force
+# # Install innoextract locally
+# Invoke-WebRequest -URI "https://github.com/dscharrer/innoextract/releases/download/1.9/innoextract-1.9-windows.zip" -OutFile "${env:RCM_CHECKOUT}\tmp\innoextract.zip"
+# Expand-Archive -LiteralPath "${env:RCM_CHECKOUT}\tmp\innoextract.zip" -DestinationPath "${env:RCM_CHECKOUT}\tmp\innoextract" -Force
 
 # Download turbovnc
 Invoke-WebRequest -URI "${env:TURBOVNC_DOWNLOAD}/${env:TURBOVNC_VERSION}/TurboVNC-${env:TURBOVNC_VERSION}-x64.exe" -OutFile "${env:RCM_CHECKOUT}\tmp\turbovnc.exe"
 
 # Extract from exe
-Push-Location "${env:RCM_CHECKOUT}\tmp"
-.\innoextract\innoextract turbovnc.exe
-Pop-Location
+turbovnc.exe /SILENT /DIR="${env:RCM_CHECKOUT}\tmp\app"
+# Push-Location "${env:RCM_CHECKOUT}\tmp"
+# .\innoextract\innoextract turbovnc.exe
+# Pop-Location
 
 # Copy extracted file to `"${env:RCM_CHECKOUT}\${env:TURBOVNC_EXTERNAL}"`
 if (Test-Path "${env:RCM_CHECKOUT}\${env:TURBOVNC_EXTERNAL}") {
@@ -20,5 +21,3 @@ if (Test-Path "${env:RCM_CHECKOUT}\${env:TURBOVNC_EXTERNAL}") {
 }
 New-Item -ItemType Directory -Force -Path "${env:RCM_CHECKOUT}\${env:TURBOVNC_EXTERNAL}"
 Move-Item -Path "${env:RCM_CHECKOUT}\tmp\app" -Destination "${env:RCM_CHECKOUT}\${env:TURBOVNC_EXTERNAL}\bin"
-
-
