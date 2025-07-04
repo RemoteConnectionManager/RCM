2f0135e68535b94dac2a0759332ba964f7c98113
Giuncan <andrea.giunchi.93@gmail.com>
2025-07-04 09:46:15 +0000

    ci: try fixing python 3.10 in macos 15

--- scripts/ci/02-install-python.sh
+++ scripts/ci/02-install-python.sh
@@ -3,4 +3,10 @@
 if [ ! -d "${PYENV_ROOT:?}" ]; then
     git clone -b "v${PYENV_VERSION:?}" "https://github.com/pyenv/pyenv.git" "${PYENV_ROOT:?}"
 fi
+
+# https://github.com/pyenv/pyenv/issues/3211
+if [[ $( sw_vers -productVersion 2>/dev/null ) ==  15.* ]]; then
+    export PYTHON_BUILD_HOMEBREW_OPENSSL_FORMULA="openssl@3"
+fi
+
 pyenv install -s "${PYTHON_VERSION:?}"
